7a8c7f93abfee0304b960e13df69a52f75d91915
Prayash Shakya <prayashshakya20@gmail.com>
2025-05-22 16:24:24 +0545

    queen, bishop, knight legal moves added

--- chessengine.py
+++ chessengine.py
@@ -1,14 +1,14 @@
 class GameState():
     def __init__(self):
         self.board = [
-            ["bR","bN","bB","--","bK","bB","bN","bR" ],
-            ["bp","bp","bp","--","bp","bp","bp","bp" ],
-            ["--","--","--","wR","bR","--","--","--" ],
+            ["bR","bN","bB","bQ","bK","bB","bN","bR" ],
+            ["bp","bp","bp","bp","bp","bp","bp","bp" ],
             ["--","--","--","--","--","--","--","--" ],
-            ["--","--","--","wR","--","--","bR","--" ],
             ["--","--","--","--","--","--","--","--" ],
-            ["wp","wp","wp","--","wp","wp","wp","wp" ],
-            ["wR","wN","wB","--","wK","wB","wN","wR" ]
+            ["--","--","--","--","--","--","--","--" ],
+            ["--","--","--","--","--","--","--","--" ],
+            ["wp","wp","wp","wp","wp","wp","wp","wp" ],
+            ["wR","wN","wB","wQ","wK","wB","wN","wR" ]
         ]
         self.moveFunctions = {'p':self.getPawnMoves, 'R':self.getRookMoves
                               ,'N': self.getKnightMoves, 'B': self.getBishopMoves, 'Q':self.getQueenMoves,
@@ -69,8 +69,6 @@ class GameState():
                         moves.append(Move((r,c), (r+1,c+1), self.board))
 
     
-
-
     def getRookMoves(self, r, c, moves):
 
         if self.whiteToMove:
@@ -100,13 +98,116 @@ class GameState():
 
 
     def getKnightMoves(self, r, c, moves):
-        pass
+        if self.whiteToMove:
+            ally, enemy = 'w', 'b'
+        else:
+            ally, enemy = 'b', 'w'
+
+        # 2) all 8 L-shaped jumps
+        knight_offsets = [
+            ( 2,  1), ( 2, -1),
+            (-2,  1), (-2, -1),
+            ( 1,  2), ( 1, -2),
+            (-1, 2), (-1, -2)
+        ]
+
+        for dr, dc in knight_offsets:
+            end_row, end_col = r + dr, c + dc
+            # skip off-board
+            if 0 <= end_row < 8 and 0 <= end_col < 8:
+                end_piece = self.board[end_row][end_col]
+                if end_piece == "--":
+                    # empty: legal move
+                    moves.append(Move((r, c), (end_row, end_col), self.board))
+                elif end_piece[0] == enemy:
+                    # capture possible
+                    moves.append(Move((r, c), (end_row, end_col), self.board))
+                # if it’s an ally, we just ignore it
     def getBishopMoves(self, r, c, moves):
-        pass   
+        if self.whiteToMove:
+            ally, enemy = 'w', 'b'
+        else:
+            ally, enemy = 'b', 'w'
+
+        # Four diagonal directions
+        directions = [(1, 1), (1, -1), (-1, 1), (-1, -1)]
+        
+        for dr, dc in directions:
+            for i in range(1, 8):
+                end_row = r + dr * i
+                end_col = c + dc * i
+                # off-board?
+                if not (0 <= end_row < 8 and 0 <= end_col < 8):
+                    break
+                end_piece = self.board[end_row][end_col]
+                if end_piece == "--":
+                    # empty square: legal move
+                    moves.append(Move((r, c), (end_row, end_col), self.board))
+                else:
+                    # occupied: if it’s an enemy, we can capture it, then stop
+                    if end_piece[0] == enemy:
+                        moves.append(Move((r, c), (end_row, end_col), self.board))
+                    # whether ally or enemy, we cannot go further
+                    break   
     def getQueenMoves(self, r, c, moves):
-        pass     
+        if self.whiteToMove:
+            ally, enemy = 'w', 'b'
+        else:
+            ally, enemy = 'b', 'w'
+
+        directions = [
+            ( 1,  0),  # down
+            (-1,  0),  # up
+            ( 0,  1),  # right
+            ( 0, -1),  # left
+            ( 1,  1),  # down-right
+            ( 1, -1),  # down-left
+            (-1,  1),  # up-right
+            (-1, -1)   # up-left
+        ]
+
+        for dr, dc in directions:
+            for i in range(1, 8):
+                end_row = r + dr * i
+                end_col = c + dc * i
+                # off-board?
+                if not (0 <= end_row < 8 and 0 <= end_col < 8):
+                    break
+                end_piece = self.board[end_row][end_col]
+                if end_piece == "--":
+                    # empty square: legal move
+                    moves.append(Move((r, c), (end_row, end_col), self.board))
+                else:
+                    # occupied: capture if enemy, then stop
+                    if end_piece[0] == enemy:
+                        moves.append(Move((r, c), (end_row, end_col), self.board))
+                    break     
     def getKingMoves(self, r, c, moves):
-        pass
+        if self.whiteToMove:
+            ally, enemy = 'w', 'b'
+        else:
+            ally, enemy = 'b', 'w'
+
+        # 2) all eight directions, but only one step
+        directions = [
+            ( 1,  0),  # down
+            (-1,  0),  # up
+            ( 0,  1),  # right
+            ( 0, -1),  # left
+            ( 1,  1),  # down-right
+            ( 1, -1),  # down-left
+            (-1,  1),  # up-right
+            (-1, -1)   # up-left
+        ]
+
+        for dr, dc in directions:
+            end_row, end_col = r + dr, c + dc
+            # ensure we're still on the board
+            if 0 <= end_row < 8 and 0 <= end_col < 8:
+                end_piece = self.board[end_row][end_col]
+                # if empty, or occupied by enemy, it's a legal move
+                if end_piece == "--" or end_piece[0] == enemy:
+                    moves.append(Move((r, c), (end_row, end_col), self.board))
 
 
 class Move():
