8a1d13c6905c299daf73f63668d205b424e18fcb
Prayash Shakya <prayashshakya20@gmail.com>
2025-05-24 22:46:38 +0545

    considers checks for valid moves

--- chessengine.py
+++ chessengine.py
@@ -15,13 +15,21 @@ class GameState():
                               'K': self.getKingMoves}                                                                       
         self.whiteToMove = True
         self.movelog = []
-        self.castleRights = True
+        # self.castleRights = True
+        self.whiteKingLocation = (7, 4)
+        self.blackKingLocation = (0, 4)
 
     def makeMove(self, move):
         self.board[move.startRow][move.startCol] = "--"
         self.board[move.endRow][move.endCol] = move.pieceMoved
-        self.movelog.append(move)
+        self.movelog.append(move)       
         self.whiteToMove = not self.whiteToMove
+
+        #update king location
+        if move.pieceMoved == 'wK':
+            self.whiteKingLocation = (move.endRow, move.endCol)
+        if move.pieceMoved == 'bK':
+            self.blackKingLocation = (move.endRow, move.endCol)
 
     def undoMove(self):
         if len(self.movelog) != 0:
@@ -29,9 +37,49 @@ class GameState():
             self.board[move.startRow][move.startCol] = move.pieceMoved
             self.board[move.endRow][move.endCol] = move.pieceCaptured
             self.whiteToMove = not self.whiteToMove
+            #update king position
+            if move.pieceMoved == 'wK':
+                self.whiteKingLocation = (move.startRow, move.startCol)
+            if move.pieceMoved == 'bK':
+                self.blackKingLocation = (move.startRow, move.startCol)
+
 
     def getValidMoves(self):#considering checks
-        return self.getAllPossibleMoves()
+        #generate all possible move
+        moves = self.getAllPossibleMoves()
+
+        #make the move
+        for i in range(len(moves)-1, -1, -1): #going backwards because of possible remove bug
+            self.makeMove(moves[i])
+
+            #generate all opponent moves
+            
+            #see if the king is attacked. if attacked, not valid
+            self.whiteToMove = not self.whiteToMove #the make move function switches turns. so need to switch back
+            if self.inCheck():
+                moves.remove(moves[i])
+            self.whiteToMove = not self.whiteToMove #to counteract the undomove which also switches turns
+            self.undoMove()
+
+        return moves
+    
+    #determine if the current player is in check
+    def inCheck(self):
+        if self.whiteToMove:
+            return self.squareUnderAttack(self.whiteKingLocation[0], self.whiteKingLocation[1])
+        else:
+            return self.squareUnderAttack(self.blackKingLocation[0], self.blackKingLocation[1])
+
+    #determing if square r, c is under attack
+    def squareUnderAttack(self, r, c):
+        self.whiteToMove = not self.whiteToMove
+        oppMoves = self.getAllPossibleMoves()
+        self.whiteToMove = not self.whiteToMove #switch back turn
+        for move in oppMoves:
+            if move.endRow == r and move.endCol == c:
+                return True
+        return False
+        
 
     def getAllPossibleMoves(self):#not considering checks
         moves = []
